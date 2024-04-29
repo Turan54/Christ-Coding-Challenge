dc68ea0d4966804794461a156f5bb79b0a15604f
ST <donkralle@outlook.de>
2024-04-29 16:58:20 +0200

    Update ProductDto.cs

--- Christ Coding Challenge/Models/ProductDto.cs
+++ Christ Coding Challenge/Models/ProductDto.cs
@@ -5,21 +5,21 @@ namespace Christ_Coding_Challenge.Models
 {
     public class Article
     {
-        public string Id { get; set; } // Eindeutige ID des Artikels
-        public string ArticleId { get; set; } // Artikelnummer
-        public List<ArticleAttribute> Attributes { get; set; } = new List<ArticleAttribute>(); // Liste der Attribute
+        public string Id { get; set; } 
+        public string ArticleId { get; set; } 
+        public List<ArticleAttribute> Attributes { get; set; } = new List<ArticleAttribute>(); 
     }
 
     public class ArticleAttribute
     {
-        public int ArticleAttributeId { get; set; }  // Primary
-        public string Key { get; set; } // Schlüssel für das Attribut
-        public string Source { get; set; } // Quelle des Attributs
-        public string Value { get; set; } // Wert des Attributs
-        public string Label { get; set; } // Bezeichnung des Attributs
-        public string? Language { get; set; } // Sprache des Attributs, kann null sein
-        public string ArticleId { get; set; } // Fremdschlüssel zur Article
-        public Article Article { get; set; } // Navigationseigenschaft zurück zu Article
+        public int ArticleAttributeId { get; set; }  
+        public string Key { get; set; } 
+        public string Source { get; set; } 
+        public string Value { get; set; } 
+        public string Label { get; set; } 
+        public string? Language { get; set; } 
+        public string ArticleId { get; set; } 
+        public Article Article { get; set; }
     }
 
     public class ArticleDbContext : DbContext
@@ -34,25 +34,25 @@ namespace Christ_Coding_Challenge.Models
         {
             optionsBuilder.UseSqlServer(
               "Data Source=.\\SQLEXPRESS;Initial Catalog=MyApplicationDB;Integrated Security=True;Trust Server Certificate=True",
-                options => options.CommandTimeout(180)); // Setzt das Command Timeout auf 180 Sekunden
+                options => options.CommandTimeout(180)); 
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Definiere den Primärschlüssel für ArticleAttribute
+            
             modelBuilder.Entity<ArticleAttribute>().HasKey(aa => aa.ArticleAttributeId);
 
-            // Konfiguriere die 1:n-Beziehung zwischen Article und ArticleAttribute
+            
             modelBuilder.Entity<Article>()
                 .HasMany(a => a.Attributes)
                 .WithOne(aa => aa.Article)
-                .HasForeignKey(aa => aa.ArticleId); // Stelle sicher, dass der Fremdschlüssel auf ArticleId gesetzt ist
+                .HasForeignKey(aa => aa.ArticleId); 
         }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowSpecificOrigin",
-                    builder => builder.WithOrigins("http://localhost:8080") // Erlaubt Zugriffe vom Frontend
+                    builder => builder.WithOrigins("http://localhost:8080") 
                                       .AllowAnyMethod()
                                       .AllowAnyHeader());
             });
